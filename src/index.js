b6ea3dd4e4a1ba2ff0dcb8d60db5bdb1fa7a7061
SantoshK96 <santoshkumarp1996@gmail.com>
2020-07-18 18:10:10 +0530

    Second Commit : Added redux changes with redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';//helps to get instant logs in console
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-  <App />
+  <Provider store={store} >
+    <App />
+  </Provider>
   ,
   document.getElementById('root')
 );
